ffd4b9730ea03a90456368d4b6c3366801ae0166
Eduard-soft <92379627+Eduard-soft@users.noreply.github.com>
2021-10-24 16:06:38 +0300

    current state

--- EduardMudriyskiiHW 2.2/ViewController.swift
+++ EduardMudriyskiiHW 2.2/ViewController.swift
@@ -8,12 +8,52 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBOutlet weak var redSlider: UISlider! {
+        didSet{
+            redSlider.tintColor = UIColor.red
+        }
+    }
+    
+    @IBOutlet weak var greenSlider: UISlider! {
+        didSet{
+            greenSlider.tintColor = UIColor.green
+        }
+    }
+    
+    @IBOutlet weak var blueSlider: UISlider! {
+        didSet{
+            blueSlider.tintColor = UIColor.blue
+        }
+    }
+    
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    func changeTextLabel() {
+        redLabel.text = String(format: "%.2f", redSlider.value)
+        greenLabel.text = String(format: "%.2f", greenSlider.value)
+        blueLabel.text = String(format: "%.2f", blueSlider.value)
+    }
+    
+    
+    
+    
+    
+    func changeColor() {
+        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
     }
 
-
+    @IBAction func rgbSliderChanged(_ sender: Any) {
+        changeTextLabel()
+        changeColor()
+    }
 }
 
